bc720d588f96ab84cf21144b5c5624cf672bd7c7
Aniket <vishnoianiket4@gmail.com>
2024-04-12 18:56:55 +0530

    responsive

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import React from "react";
 import { FaRegNewspaper } from "react-icons/fa6";
 import { FaRegCircleUser } from "react-icons/fa6";
--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import React, { useState } from "react";
 import { MdOutlineDashboard } from "react-icons/md";
 import { FaRegUser } from "react-icons/fa";
