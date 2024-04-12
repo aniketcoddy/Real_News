40c8d8c8c918afce351b107ebf9fa7dc1b23243b
Aniket <vishnoianiket4@gmail.com>
2024-04-12 18:40:23 +0530

    responsivenes done

--- app/Post/[PostDetails]/page.jsx
+++ app/Post/[PostDetails]/page.jsx
@@ -8,35 +8,34 @@ import Link from "next/link";
 const DetailsPage = () => {
   return (
     <div className="bg-[#F9F9F9] p-1 font-[Geist]  lg:col-start-2 lg:col-end-3 lg:row-start-2 lg:row-end-3 col-start-1 col-end-3 row-start-2 row-end-3">
-      <div className="border-[1px] flex m-7 gap-6 mx-14 flex-col  rounded-md bg-white border-[#D7D7D7] lg:p-4 p-3">
-        <div className="flex justify-between">
-          <h1 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl text-[#141417] line-clamps-1 font-medium font-Geist">
-           {` Any leads or referrals please!`}
+      <div className="border-[1px] flex mt-7 gap-3 p-3 xl:m-7 xl:gap-6 mx-7 xl:mt-16 flex-col w-[300px] xs:w-[408px] xm:w-[488px] sm:w-[582px] md:w-[708px] xl:w-[929px] 2xl:w-[1190px] 3xl:w-[1535px] rounded-md bg-white border-[#D7D7D7] lg:p-8">
+        <div>
+          <h1 className=" text-[9px] xs:text-[10px] xm:text-[12px] md:text-lg sm:text-sm  xl:text-[22px] 2xl:text-3xl 3xl:text-[36px] 3xl:leading-[56.5px] w-72 xs:w-[372px] xm:w-[436px] sm:w-[482px] md:w-[629px] xl:w-[778px] 2xl:w-[1031px] 3xl:w-[1283px] text-[#141417]  font-normal font-Geist">
+            {`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy`}
           </h1>
         </div>
-        <p className="text-xs sm:text-sm md:text-base  font-Geist text-[#3F3F42] line-clamp-2 ">
-          {`Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry.
-          <br /> Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s,`}
+        <p className="text-[6px] xs:text-[9px] xm:text-[10px] sm:text-xs md:text-[15px] xl:text-lg 2xl:text-2xl 3xl:text-[31px] font-normal md:leading-6 xl:leading-8 2xl:leading-9 3xl:leading-[50px] w-[257px] xs:w-[368px]  xm:w-[422px] sm:w-[511px] md:w-[639px] xl:w-[767px] 2xl:w-[1037px] 3xl:w-[1307px] font-Geist text-[#3F3F42]">
+          {`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.,`}
         </p>
-        <span className="text-xs font-Geist text-[#4A94FA]">
-          {`#master #abroad #usa`}
+        <span className=" text-[6px] xs:text-[9px] xm:text-[10px] md:text-[15px] xl:text-lg sm:text-xs font-Geist 2xl:text-2xl 3xl:text-3xl  text-[#4A94FA]">
+          {` #master #abroad #usa`}
         </span>
-        <Image
-          src="/NMIMS.jpg"
-          alt="pic"
-          width={400}
-          height={400}
-          className="object-contain rounded-md"
-        />
+
+        <div className="flex items-center rounded-lg justify-center w-[271px] h-[410px] xs:w-[380px] xs:h-[571px] xm:w-[463px] xm:h-[696px] sm:w-[555px] sm:h-[831px]  md:w-[677px] md:h-[1024px] lg:w-[654px] lg:h-[987px] xl:w-[863px] xl:h-[1321px] 2xl:w-[1116px] 2xl:h-[1677px] 3xl:w-[1455px] 3xl:h-[2175px] overflow-hidden relative">
+          <Image
+            src="/NMIMS.jpg"
+            alt="post"
+            className="absolute inset-0 w-full h-full object-cover"
+            fill
+          />
+        </div>
       </div>
 
       {/* lesson */}
-      <div className="border-[1px] flex m-7 mx-14 gap-3 rounded-md bg-white border-[#D7D7D7] lg:p-4 p-3">
+      <div className="border-[1px] flex p-3 xl:m-7 m-7 mx-7 w-[300px] xs:w-[408px] xm:w-[488px] sm:w-[582px] md:w-[708px] xl:w-[929px] 2xl:w-[1190px] 3xl:w-[1535px] rounded-sm bg-white border-[#D7D7D7] lg:p-8">
         <span className="flex text-6xl">“</span>
-        <p className="text-sm mt-4">
-         {` Seeing 100% placement reports from B-schools, you might assume that
+        <p className="text-sm xl:text-base 2xl:text-lg  mt-4">
+          {` Seeing 100% placement reports from B-schools, you might assume that
           it’s worth pursuing MBA & you will definitely get placed at the end,
           but the reality is completely different. It’s time you start thinking
           and stop falling for not so true placement stats of B-schools`}
@@ -44,13 +43,13 @@ const DetailsPage = () => {
       </div>
 
       {/* comment section */}
-      <span className="text-xs font-medium font-Geist m-7 mx-14 text-[#4A94FA] ">
-       {` It’s time to understand & rethink by reading these comments`}
+      <span className="flex justify-start text-[10px] sm:text-sm md:text-base 2xl:text-lg m-7 mx-7 text-[#4A94FA] ">
+        {` It’s time to understand & rethink by reading these comments`}
       </span>
 
-      <div className="border-[1px] flex m-4 mx-14 gap-3 rounded-md bg-white border-[#D7D7D7] lg:p-4 p-3">
-        <p className="text-sm">
-         {` They make you sign a document before placements starts. Which
+      <div className="border-[1px] flex p-3 xl:m-7 m-7 mx-7 w-[300px] xs:w-[408px] xm:w-[488px] sm:w-[582px] md:w-[708px] xl:w-[929px] 2xl:w-[1190px] 3xl:w-[1535px] rounded-md bg-white border-[#D7D7D7] lg:p-8">
+        <p className="text-sm xl:text-base 2xl:text-lg ">
+          {` They make you sign a document before placements starts. Which
           basically says that the placement team can do whatever the f they want
           and you agree to it because without it you are debarred from sitting
           for placements. Messed up system`}
@@ -62,8 +61,8 @@ const DetailsPage = () => {
         <div className="absolute border-[1px] h-36 w-28 mt-20 ml-24 border-r-0 border-[#D7D7D7]"></div>
         <div className="absolute border-[1px] h-[400px] w-28 mt-20 ml-[70px] border-r-0 border-[#D7D7D7]"></div>
 
-        <div className="border-[1px] flex m-7 mx-14 gap-3 rounded-md z-50 bg-white border-[#D7D7D7] lg:p-4 p-3">
-          <p className="text-sm">
+        <div className="border-[1px] flex p-3 xl:m-7 m-7 mx-7 w-[300px] xs:w-[408px] xm:w-[488px] sm:w-[582px] md:w-[708px] xl:w-[929px] 2xl:w-[1190px] 3xl:w-[1535px] rounded-md bg-white border-[#D7D7D7] lg:p-8">
+          <p className="text-sm xl:text-base 2xl:text-lg ">
             {`So I know from very close sources that the folks who generally are
             asked to opt out are those who have been unable to get any
             placements even after 2-3 months of attending interviews. These are
@@ -77,23 +76,22 @@ const DetailsPage = () => {
           </p>
         </div>
 
-      
-        <div className="border-[1px] z-50 flex m-2   mx-14 ml-32 gap-3 rounded-md bg-white border-[#D7D7D7] lg:p-4 p-3">
-          <p className="text-sm ">
-           {` Agree to everything. But then that clearly means that they
+        <div className="border-[1px] flex p-3 xl:m-7 m-7 mx-7 w-[300px] xs:w-[408px] xm:w-[488px] sm:w-[582px] md:w-[708px] xl:w-[929px] 2xl:w-[1190px] 3xl:w-[1535px] rounded-md bg-white border-[#D7D7D7] lg:p-8">
+          <p className="text-sm xl:text-base 2xl:text-lg ">
+            {` Agree to everything. But then that clearly means that they
             don't/can't provide 100% placements. Which is the primary reason
             this "opt-out" scheme is being implemented in the first place`}
           </p>
         </div>
-  <div className="absolute border-[1px] h-32 w-28 mt-52 ml-40 border-r-0 border-t-0 border-[#D7D7D7]"></div>
-        <div className="border-[1px] z-50 flex m-7 mx-14 ml-44 gap-3 rounded-md bg-white border-[#D7D7D7] lg:p-4 p-3">
-          <p className="text-sm ">
+        <div className="absolute border-[1px] h-32 w-28 mt-52 ml-40 border-r-0 border-t-0 border-[#D7D7D7]"></div>
+        <div className="border-[1px] flex p-3 xl:m-7 m-7 mx-7 w-[300px] xs:w-[408px] xm:w-[488px] sm:w-[582px] md:w-[708px] xl:w-[929px] 2xl:w-[1190px] 3xl:w-[1535px] rounded-md bg-white border-[#D7D7D7] lg:p-8">
+          <p className="text-sm xl:text-base 2xl:text-lg ">
             {`Oh yes that's true. They take in a lot of people but do not have
             true 100% placements. 100% is only after the optouts.`}
           </p>
         </div>
-        <div className="border-[1px] flex z-50 m-7 mx-14 ml-24 gap-3 rounded-md bg-white border-[#D7D7D7] lg:p-4 p-3">
-          <p className="text-sm ">
+        <div className="border-[1px] flex p-3 xl:m-7 m-7 mx-7 w-[300px] xs:w-[408px] xm:w-[488px] sm:w-[582px] md:w-[708px] xl:w-[929px] 2xl:w-[1190px] 3xl:w-[1535px] rounded-md bg-white border-[#D7D7D7] lg:p-8">
+          <p className="text-sm xl:text-base 2xl:text-lg ">
             {`This is nothing new. I know of three other MBA colleges that did
             something similar with the '23 batch. I was part of one. They make
             you sign a document before placements with unethical rules such as
@@ -108,14 +106,14 @@ const DetailsPage = () => {
         </div>
       </div>
 
-      <div className="border-[1px] flex m-7 mx-14 gap-3 rounded-md bg-white border-[#D7D7D7] lg:p-4 p-3">
-        <p className="text-sm ">
-         {` Indian colleges are all about placements, they will do anything to
+      <div className="border-[1px] flex p-3 xl:m-7 m-7 mx-7 w-[300px] xs:w-[408px] xm:w-[488px] sm:w-[582px] md:w-[708px] xl:w-[929px] 2xl:w-[1190px] 3xl:w-[1535px] rounded-md bg-white border-[#D7D7D7] lg:p-8">
+        <p className="text-sm xl:text-base 2xl:text-lg ">
+          {` Indian colleges are all about placements, they will do anything to
           make their stats look good.`}
         </p>
       </div>
-      <div className="border-[1px] flex m-7 mx-14 gap-3 rounded-md bg-white border-[#D7D7D7] lg:p-4 p-3">
-        <p className="text-sm ">
+      <div className="border-[1px] flex p-3 xl:m-7 m-7 mx-7 w-[300px] xs:w-[408px] xm:w-[488px] sm:w-[582px] md:w-[708px] xl:w-[929px] 2xl:w-[1190px] 3xl:w-[1535px] rounded-md bg-white border-[#D7D7D7] lg:p-8">
+        <p className="text-sm xl:text-base 2xl:text-lg ">
           {`This is great example that students, aspirants and parents should not
           blankly look at the brochure but take feedback from recent graduates
           as well to get the check of reality.`}
@@ -128,15 +126,15 @@ const DetailsPage = () => {
         </p>
       </div>
 
-      <div className="border-[1px] flex m-7 mx-14 gap-3 rounded-md bg-white border-[#D7D7D7] lg:p-4 p-3">
-        <p className="text-sm ">
-         {` How is this new? This is a well-known tactic used in all biz colleges
+      <div className="border-[1px] flex p-3 xl:m-7 m-7 mx-7 w-[300px] xs:w-[408px] xm:w-[488px] sm:w-[582px] md:w-[708px] xl:w-[929px] 2xl:w-[1190px] 3xl:w-[1535px] rounded-md bg-white border-[#D7D7D7] lg:p-8">
+        <p className="text-sm xl:text-base 2xl:text-lg ">
+          {` How is this new? This is a well-known tactic used in all biz colleges
           across the country where placement isn't 100%, even the newer IIMs.`}
         </p>
       </div>
 
-      <div className="border-[1px] flex m-7 mx-14 gap-3 rounded-md bg-white border-[#D7D7D7] lg:p-4 p-3">
-        <p className="text-sm ">
+      <div className="border-[1px] flex p-3 xl:m-7 m-7 mx-7 w-[300px] xs:w-[408px] xm:w-[488px] sm:w-[582px] md:w-[708px] xl:w-[929px] 2xl:w-[1190px] 3xl:w-[1535px] rounded-md bg-white border-[#D7D7D7] lg:p-8">
+        <p className="text-sm xl:text-base 2xl:text-lg ">
           {`MBA colleges have this thing from a long time. Just put “opted out of
           placements” for people who haven’t got placed. They have been doing
           these on their admission brochures. The placement team is formed in
--- app/Post/page.jsx
+++ app/Post/page.jsx
@@ -6,35 +6,52 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Post = () => {
+
+  
+  const truncateTitle = (inputText) => {
+    const words = inputText.split(' ');
+    if (words.length > 10) {
+      return words.slice(0, 10).join(' ') + '...';
+    }
+    return inputText;
+  };
+
+  const truncateDes = (inputText) => {
+    const words = inputText.split(' ');
+    if (words.length > 26) {
+      return words.slice(0, 26).join(' ') + '...';
+    }
+    return inputText;
+  };
+
   return (
     <div className="bg-[#F9F9F9] p-1 font-[Geist]  lg:col-start-2 lg:col-end-3 lg:row-start-2 lg:row-end-3 col-start-1 col-end-3 row-start-2 row-end-3">
-      <div className="p-4 flex font-[Geist] flex-col lg:mx-8 gap-6">
+      <div className="p-4 flex w-[350px] xs:w-[440px] xm:w-[530px] sm:w-[617px] md:w-[732px] xl:w-[929px] 2xl:w-[1172px] 3xl:w-[1555px] flex-col lg:mx-8  gap-6">
         <div className="flex justify-end">
           <Link href="/Admin">
-            <span className="text-xs font-Geist text-[#0066FF] font-medium">
+            <span className="font-Geist text-[#0066FF] font-medium text-xs md:text-[14px]  xl:text-lg 2xl:text-xl 3xl:text-2xl">
               Create a Post
             </span>
           </Link>
         </div>
-        <Link href="/Post/1" className="border-[1px] flex flex-col gap-3 rounded-md bg-white border-[#D7D7D7] lg:p-6 p-3">
+        <Link href="/Post/1" className="border-[1px] flex flex-col xl:gap-5 gap-2 w-[322px] xs:w-[421px] xm:w-[506px] sm:w-[589px] md:w-[713px]  xl:w-[897px] 2xl:w-[1161px] 3xl:w-[1548px] rounded-md bg-white border-[#D7D7D7] xl:p-10 p-4 ">
           <div className="flex justify-between">
-            <h1 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl text-[#141417] line-clamps-1 font-medium font-Geist">
-              {`Any leads or referrals please!`}
+            <h1 className=" text-[9px] xs:text-[10px] xm:text-[12px] md:text-lg sm:text-sm xl:text-[22px] 2xl:text-3xl 3xl:text-[42px] text-[#141417]  font-normal font-Geist">
+              {truncateTitle(`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,`)}
             </h1>
-            <div className="flex gap-3">
-              <FiEdit className="xl:w-5 xl:h-5" />
-              <IoEyeOutline className="xl:w-5 xl:h-5" />
+            <div className="flex xl:gap-6 gap-3">
+              <FiEdit className=" w-3 h-3 sm:w-4 sm:h-4 xl:w-5 xl:h-5 2xl:w-6 2xl:h-6 3xl:w-8 3xl:h-8" />
+              <IoEyeOutline className=" w-3 h-3 sm:w-4 sm:h-4 xl:w-5 xl:h-5 2xl:w-6 2xl:h-6 3xl:w-8 3xl:h-8" />
             </div>
           </div>
-          <p className="text-xs sm:text-sm md:text-base  font-Geist text-[#3F3F42] line-clamp-2 ">
-           {` Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the <br /> industry's standard dummy
-            text ever since the 1500s,`}
+          <p className="text-[7px] xs:text-[9px] xm:text-[10px] sm:text-xs md:text-[15px] xl:text-lg 2xl:text-[23px] 3xl:text-[31px] font-normal md:leading-7 xl:leading-8 2xl:leading-9 3xl:leading-[50px] w-[282px] xs:w-[366px] xm:w-[453px] sm:w-[511px] md:w-[639px] xl:w-[767px] 2xl:w-[1037px] 3xl:w-[1399px] font-Geist text-[#3F3F42]">
+
+          {truncateDes(`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.,`)}
           </p>
-          <span className="text-xs font-Geist text-[#4A94FA]">
+          <span className=" text-[7px] xs:text-[9px] xm:text-[10px] md:text-[15px] xl:text-lg sm:text-xs font-Geist 2xl:text-[23px] 3xl:text-3xl  text-[#4A94FA]">
            {` #master #abroad #usa`}
-          </span>
-          <div className="flex items-center rounded-md justify-center w-72 h-36 sm:w-[480px] sm:h-[240px]  md:w-[580px] md:h-[290px] xl:w-[780px] xl:h-[350px]  overflow-hidden relative">
+          </span>                             
+          <div className="flex items-center rounded-md justify-center w-[266px] h-[123px] xs:w-[362px] xs:h-[163px] xm:w-[449px] xm:h-[205px] sm:w-[526px] sm:h-[240px]  md:w-[644px] md:h-[288px] xl:w-[766px] xl:h-[351px] 2xl:w-[1026px] 2xl:h-[455px] 3xl:w-[1399px] 3xl:h-[635px] overflow-hidden relative">
             <Image
               src="/NMIMS.jpg"
               alt="post"
@@ -43,11 +60,11 @@ const Post = () => {
             />
           </div>
           <div className="flex gap-3">
-            <div className=" border-[1px] border-[#D7D7D7] p-1  text-gray-500 font-medium  px-2 rounded-md flex items-center gap-1">
-              <FaRegComment className="w-3 h-3 xl:w-5 xl:h-5" />
-              <p className="text-xs text-[#707680] xl:text-sm">2</p>
+            <div className=" border-[1px] border-[#D7D7D7] p-1  text-gray-500  px-2 rounded-md flex justify-center items-center gap-1">
+              <FaRegComment className="w-3 h-3 sm:w-4 sm:h-4 xl:w-5 xl:h-5 2xl:w-6 2xl:h-6" />
+              <p className="text-[12px] text-[#707680] xl:text-base 2xl:text-lg ">2</p>
             </div>
-            <p className="border-[1px] xl:text-sm border-[#D7D7D7] text-[#707680] font-medium  p-1 px-2 rounded-md text-xs">
+            <p className="border-[1px] text-[12px] border-[#D7D7D7] xl:text-base 2xl:text-lg  text-[#707680]  p-1 px-2 rounded-md text-xs">
              {` April,24`}
             </p>
           </div>
